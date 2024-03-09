c20636eb0d00396f54e2f60e272d7409a1c31bd4
Louis <louisstein16@gmail.com>
2024-03-09 15:54:58 +0100

    Add cmBalance state and useEffect hook to display Crypto Market balance

--- src/app/[components]/[leftPanel]/leftPanel.js
+++ src/app/[components]/[leftPanel]/leftPanel.js
@@ -1,15 +1,36 @@
 import strucStyle from "./../../styles.module.css";
 import style from "./leftPanel.module.css";
 import FlashLoaner from "./flashLoaner";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { getProvider } from "./../../../../ethereum.js";
 
 export default function LeftPanel({ contract, getBalance }) {
+  const [cmBalance, setCmBalance] = useState(0.0);
+
+  useEffect(() => {
+    async function dodo() {
+      if (contract) {
+        let cm = ethers.utils.formatEther(
+          (
+            await getProvider().getBalance(
+              "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+            )
+          ).toString()
+        );
+        setCmBalance(cm);
+      }
+    }
+    dodo();
+  }, [contract, getBalance]);
+
   return (
     <div className={strucStyle.LeftPanel}>
       <div className={style.rw}>
         <div className={style.position}>
           <div className={style.cm}>
             <div className={style.title}>Crypto Market</div>
-            <div className={style.cmContent}>Test</div>
+            <div className={style.cmContent}>{cmBalance} ETH</div>
           </div>
           <FlashLoaner contract={contract} getBalance={getBalance} />
         </div>
